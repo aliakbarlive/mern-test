fe5710968deb9ea15d794689a81684dac0b00cb5
Ali Akbar <aliakbarlive14@gmail.com>
2024-11-12 12:01:51 +0500

    updated

--- backend/server.js
+++ backend/server.js
@@ -2,12 +2,14 @@ const express = require('express');
 const connectDB = require('./config/db');
 require('dotenv').config();
 const authRoutes = require('./routes/auth.route')
+const carRoutes= require('./routes/car.routes')
 const app = express();
 const PORT = process.env.PORT
 connectDB();
 
 app.use(express.json());
 app.use('/api/auth', authRoutes);
+app.use('/api/car',carRoutes)
 
 app.listen(PORT, () => {
     console.log('Server is running on port ' + PORT);
